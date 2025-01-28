bb69f481831227af152d2a23dc08d4167db8601b
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-01-28 15:31:04 -0800

    Scaffold nav bar + add basic styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,21 @@ import CartIcon from "../../assets/shopping-cart.svg"; // from Lucide
 import styles from "./App.module.css";
 
 function App() {
+  const itemsInCart = 0; // will be state eventually
+
   return (
     <>
       <h1>Fake Store</h1>
-      <nav>
+      <nav className={styles.storeNav}>
         <button type="button">Home</button>
         <button type="button">Shop</button>
-        <button type="button">
-          <img src={CartIcon} className={styles.cartIcon} alt="Cart" />
+        <button type="button" className={styles.toggleCart}>
+          <img
+            src={CartIcon}
+            className={styles.cartIcon}
+            alt={`Cart ${itemsInCart} items`}
+            title={`Cart ${itemsInCart} items`}
+          />
         </button>
       </nav>
     </>
